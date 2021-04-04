b36ac26ea4bc2f131004d43bf8efac181891edbd
MarcLopezBermudo <79195514+MarcLopezBermudo@users.noreply.github.com>
2021-04-04 18:12:15 +0200

    Update pn532_puzzle2.py

--- pn532_puzzle2.py
+++ pn532_puzzle2.py
@@ -1,66 +1,73 @@
 import gi
-import sys
 import threading
 
 gi.require_version("Gtk","3.0")
-from gi.repository import Gtk, Gdk, GObject
+from gi.repository import Gtk, Gdk,GdkPixbuf
 from pn532_puzzle1 import *
 
-class Finestra(Gtk.Window):
+
+class Window(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self,title="Puzzle2")
-        
-        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
+        # Window
+        Gtk.Window.__init__(self,title="Puzzle2", decorated=True, name="Window")
+        self.set_default_size(500, 200)
+        # Box
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20,name="Box")
         self.add(self.box)
         
-        self.event_box = Gtk.EventBox()
-        self.event_box.override_background_color(Gtk.StateFlags.NORMAL,Gdk.RGBA(0,0,8,1))
-        
-        self.label = Gtk.Label(label='<span foreground="white" size="x-large">Please, login with your UID</span>')
-        self.label.set_use_markup(True)
+        # UPC Image
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
+            filename="image_upc.jpg", 
+            width=40, 
+            height=40, 
+            preserve_aspect_ratio=True)   
+        image = Gtk.Image.new_from_pixbuf(pixbuf)
+        self.box.add(image)
+
+        #Label
+        self.label = Gtk.Label(label='Please, login with your UID',name ="Label")
         self.label.set_size_request(500,200)
-        self.event_box.add(self.label)
-        
-        #self.image = Gtk.Image()
-        #self.image.set_from_file('./image_upc.png')
-        #self.gtk_image_set_from_icon_name (self.image,NULL,GTK_ICON_SIZE_MENU)
-        #self.event_box.add(self.image)
-        
-        self.button = Gtk.Button(label="Clear")
+        self.box.add(self.label)
+       
+        # Button
+        self.button = Gtk.Button(label="Clear",name="Button")
         self.button.connect("clicked", self.on_button_clicked)
-        
-        self.box.pack_start(self.event_box, True, True, 0)
         self.box.pack_start(self.button,True, True, 0)
-    
-# Funció que es crida un cop premut el botó
+        
     def on_button_clicked(self, widget):
-        self.label.set_label('<span foreground="white" size="x-large">Please, login with your UID</span>')
-        #self.label.set_use_markup(True)
-        self.event_box.override_background_color(Gtk.StateFlags.NORMAL,Gdk.RGBA(0,0,8,1))
-        #win = Finestra()
+        self.label.set_label('Please, login with your UID')
+        #Thread
         global thread
         if not thread.isAlive():
             thread = threading.Thread(target=scan_uid)
             thread.start()
         
-# Funció que es crida per llegir l'UID amb read_uid 
 def scan_uid():
-    # Llegim UID
+    # UID
     rf = RfidPnNfc()
     uid = rf.read_uid()
-    # Canviem la label a color vermell
-    win.label.set_label('<span foreground="white" size="x-large">UID: '+uid+'</span>')
-    win.event_box.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(8,0,0,1))
+    win.label.set_label('UID: '+uid)
+    
         
-        
+def load_css(self):
+    # CSS 
+    self.style_provider = Gtk.CssProvider()
+    css_file = open("style.css","r")
+    css = css_file.read()
+    self.style_provider.load_from_data(bytes(css.encode()))
+    Gtk.StyleContext.add_provider_for_screen(
+        Gdk.Screen.get_default(), self.style_provider,
+        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+    
 if __name__ == "__main__":
-    win = Finestra()
+    win = Window()
+    load_css(win)
     thread = threading.Thread(target=scan_uid)
-    # Daemon Threads (non-blocking threads)--> s'executen independentment del thread principal
-    #thread.setDaemon(True)
     thread.start()
     win.connect("destroy", Gtk.main_quit)                   
     win.show_all()
     Gtk.main()
+        
         
         
